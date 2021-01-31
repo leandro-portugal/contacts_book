a009953629d340a86f5874e97f5aab4cf92c0aa3
Leandro Portugal <leandroportugal@outlook.com.br>
2021-01-31 15:09:18 -0300

    Creating contact and category models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=154, blank=True)
+
+
+class Contact(models.Model):
+    firs_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
+    telephone = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(max_length=20, blank=True)
+    creation_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
