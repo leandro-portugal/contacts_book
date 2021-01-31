1daa25ed8b13d6e761baaec1dc0cde916179f2b1
Leandro Portugal <leandroportugal@outlook.com.br>
2021-01-31 16:20:44 -0300

    Adding models to the admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'telephone', 'email', 'creation_date', 'category')
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+    list_per_page = 15
+    search_fields = ('first_name', 'last_name', 'telephone')
+
+
+admin.site.register(Category)
+admin.site.register(Contact, ContactAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -5,12 +5,18 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=154, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
-    firs_name = models.CharField(max_length=254, null=False, blank=False)
+    first_name = models.CharField(max_length=254, null=False, blank=False)
     last_name = models.CharField(max_length=254, null=False, blank=False)
     telephone = models.CharField(max_length=20, null=False, blank=False)
-    email = models.EmailField(max_length=20, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
     creation_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.first_name
