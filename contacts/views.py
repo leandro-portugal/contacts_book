8e42834572d5d878c32d65742d322363c505a50c
Leandro Portugal <leandroportugal@outlook.com.br>
2021-02-01 02:02:26 -0300

    Performing pagination, sorting, remove footer and filtering

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.order_by('-id').filter(
+        show=True
+    )
+    paginator = Paginator(contacts, 10)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
     return render(request, 'contacts/index.html', {
         'contacts': contacts
     })
@@ -12,6 +18,8 @@ def index(request):
 
 def show_contact(request, contact_id):
     contact = get_object_or_404(Contact, id=contact_id)
+    if not contact.show:
+        raise Http404
     return render(request, 'contacts/show_contact.html', {
         'contact': contact
     })
