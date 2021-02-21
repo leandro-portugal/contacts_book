90cc1b6d8db25ff38c26ffd80d64f77e918425a3
Leandro Portugal <leandroportugal@outlook.com.br>
2021-02-21 14:03:36 -0300

    Search contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('<int:contact_id>', views.show_contact, name='show_contact'),
 
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contact
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -22,4 +24,25 @@ def show_contact(request, contact_id):
         raise Http404
     return render(request, 'contacts/show_contact.html', {
         'contact': contact
+    })
+
+
+def search(request):
+    term = request.GET.get('term')
+    if term is None or not term:
+        raise Http404()
+    fields = Concat('first_name', Value(' '), 'last_name')
+
+    contacts = Contact.objects.annotate(
+        full_name=fields
+    ).filter(
+        Q(full_name__icontains=term) | Q(telephone__icontains=term)
+
+    )
+
+    paginator = Paginator(contacts, 10)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'contacts/search.html', {
+        'contacts': contacts
     })
